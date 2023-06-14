86868d7e7e498979948c2ea9bf34dd9b6b32cc30
Ivan <ivansamwabina@gmail.com>
2023-06-14 21:27:24 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=h1
+    , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Page</title>
+</head>
+<body>
+    <h1>This is About Page Controller</h1>
+    <a href="{{route('contact.page')}}">Contact</a>
+</body>
+</html>
