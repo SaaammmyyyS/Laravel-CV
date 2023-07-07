a6d87ac848f3dc37bd66b83ed51ee8a824a84d85
Ivan <ivansamwabina@gmail.com>
2023-07-07 22:13:17 +0800

    Added Contact Page

---
+++ app/Http/Controllers/Home/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contact;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function Contact(){
+        return view('frontend.contact');
+    } // End Method
+
+    public function StoreMessage(Request $request){
+        Contact::insert([
+            'name' => $request->name,
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'phone' => $request->phone,
+            'message' => $request->message,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Your Message Submitted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    } // End Method
+
+    public function ContactMessage(){
+        $contacts = Contact::latest()->get();
+        return view('admin.contact.allcontact', compact('contacts'));
+    }
+
+    public function DeleteMessage($id){
+        Contact::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Your Message Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+}
